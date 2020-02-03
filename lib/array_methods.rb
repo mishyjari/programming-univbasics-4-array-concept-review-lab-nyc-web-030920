a3d36131a37976d24f0a994bf4d1242d3bd22bca
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-03 20:01:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,26 +1,9 @@
 def find_element_index(array, value_to_find)
-  i = 0
-  index = nil
-  while i < array.length do 
-    if array[i] == value_to_find
-      index = i
-    else 
-      i += 1 
-  end
-  index
+
 end
 
 def find_max_value(array)
-  i = 1
-  max = array[0]
-  while i < array.length do
-    if arr[i] > max
-      max = arr[i]
-      i += 1
-    else 
-      i += 1
-  end
-  max
+
 end
 
 def find_min_value(array)

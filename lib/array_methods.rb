24b90d8f15d2c18af0d1911024e2c4a9d03949ae
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-03 20:01:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,14 +7,5 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  i = 1
-  min = arr[0]
-  while i < arr.length do
-    if arr[i] < min 
-      min = arr[i]
-      i += 1
-    else 
-      i += 1
-  end
-  min
+
 end
